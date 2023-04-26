409704d28c3ec6d35d707b6340923514e22274c1
waga43 <waga43tech@gmail.com>
2023-04-26 13:21:58 -0700

    2-export_to_JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+Script  exports data in JSON string format.
+"""
+
+from requests import get
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    json_response = response.json()
+
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    json_response2 = response2.json()
+
+    for item in json_response2:
+        if item['id'] == int(argv[1]):
+            u_name = item['username']
+            id_no = item['id']
+
+    row = []
+
+    for item in json_response:
+
+        new_dict = {}
+
+        if item['userId'] == int(argv[1]):
+            new_dict['username'] = u_name
+            new_dict['task'] = item['title']
+            new_dict['completed'] = item['completed']
+            row.append(new_dict)
+
+    final_dict = {}
+    final_dict[id_no] = row
+    json_obj = json.dumps(final_dict)
+
+    with open(argv[1] + ".json",  "w") as file:
+        file.write(json_obj)
